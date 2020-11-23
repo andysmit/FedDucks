73956783709424134ffcd3eca0f964dbc4200e41
Andy <andysmit@uvic.ca>
2020-11-23 12:13:15 -0800

    Added POST request for data entry

--- api/app/api.py
+++ api/app/api.py
@@ -1,4 +1,5 @@
 import json, falcon
+# import app.db as db
 
 class Resource(object):
     
@@ -8,3 +9,17 @@ class Resource(object):
                 }
         resp.body = json.dumps(doc, ensure_ascii=False)
         resp.status = falcon.HTTP_200
+
+class DataPage(object):
+    def on_post(self, req, resp):
+        time = req.get_param('time')
+        food = req.get_param('food')
+        location = req.get_param('location')
+        number = req.get_param('number')
+        amount = req.get_param('amount')
+        resp.status = falcon.HTTP_200
+
+    def on_get(self, req, resp): 
+        # TODO add viewing data
+        resp.status = falcon.HTTP_200
+
--- api/app/app.py
+++ api/app/app.py
@@ -1,8 +1,14 @@
 import falcon
 
-from .api import Resource
+from .api import Resource, DataPage
 
 api = application = falcon.API()
 
 images = Resource()
 api.add_route('/images', images)
+
+data = DataPage()
+api.add_route('/data', data)
+
+
+
